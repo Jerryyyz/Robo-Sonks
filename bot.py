339adf4bf673d6d321dbf97d3016ca4dcfd535de
Jerryyyx <2415465@dundee.ac.uk>
2022-10-10 19:04:08 +0100

    Added basic functionality using hikari and lightbulb library. Bot posts start message and has a slash ping command

---
+++ bot.py
@@ -0,0 +1,22 @@
+import hikari
+import lightbulb
+
+bot = lightbulb.BotApp(
+    token='MTAyOTA3OTc2MTU1NDc4NDI4Ng.GpCXwE.c5Z8OxUpJbKA-pZNdM3EbM2XQhJ-bcLKoHFLKg',
+    default_enabled_guilds=(869036831382048819)
+)
+
+
+@bot.listen(hikari.StartedEvent)
+async def bot_started(event):
+    print('Robo-Sonks has started')
+
+
+@bot.command
+@lightbulb.command('ping', 'Says pong!')
+@lightbulb.implements(lightbulb.SlashCommand)
+async def ping(ctx):
+    await ctx.respond('Pong!')
+
+bot.run()
+
