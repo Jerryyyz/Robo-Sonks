b53fc8d6ea0f5a222afbbe32bb44df6174184955
Jerryyyz <deligiannisjerry@gmail.com>
2022-10-10 23:18:59 +0100

    Revert "Changed it so KCD and 6C trigger the messages but only if the messages contain only those characters"
    This reverts commit 176927b1aac181526a9c610a7298c15c89951643.

--- bot.py
+++ bot.py
@@ -46,9 +46,9 @@ async def theme(ctx: lightbulb.Context) -> None:
 @bot.listen(hikari.GuildMessageCreateEvent)
 async def reply(event) -> None:
     if spam:
-        if event.content.upper() == 'KCD':
+        if 'SONKS KCD' in event.content.upper():
             await event.message.respond("That's my fucking job", reply=True)
-        elif event.content.upper() == '6C':
+        elif 'TERUMI 6C' in event.content.upper():
             await event.message.respond(
                 'https://cdn.discordapp.com/attachments/556756134367592481/1028413028510740561/monke_2-1.mp4',
                 reply=True
