0cffd0842a7a81ddd48203da78a5342da916245a
Jerryyyz <deligiannisjerry@gmail.com>
2022-10-11 15:49:28 +0100

    Added optional argument to kcd command, checks for user input not too large or too small, or sends a random quote as default

--- bot/extensions/kcd-command.py
+++ bot/extensions/kcd-command.py
@@ -14,7 +14,13 @@ def load(bot):
 
 
 @plugin.command
+@lightbulb.option('number', 'number of quote', type=int, required=False, default=0)
 @lightbulb.command('kcd', 'Hear a famous Sonks quote')
 @lightbulb.implements(lightbulb.SlashCommand)
 async def kcd(ctx: lightbulb.Context) -> None:
-    await ctx.respond(random.choice(quotes))
+    if ctx.options.number == 0:
+        await ctx.respond(random.choice(quotes))
+    elif ctx.options.number < 1 or ctx.options.number > len(quotes):
+        await ctx.respond('This quote number does not exist', flags=hikari.MessageFlag.EPHEMERAL)
+    else:
+        await ctx.respond(quotes[int(ctx.options.number)])
