cd9f99a00a031371464da9bea6489dc06f64e0db
Jerryyyz <deligiannisjerry@gmail.com>
2022-10-11 15:03:18 +0100

    Working react emojis

--- bot/extensions/react.py
+++ bot/extensions/react.py
@@ -3,8 +3,6 @@ import lightbulb
 
 import random
 
-from __main__ import get_emojis
-
 plugin = lightbulb.Plugin('react')
 
 
@@ -16,9 +14,9 @@ def load(bot):
 
 
 kcd_emojis = [hikari.Emoji.parse('<:zengar:828669811730022460>'), hikari.Emoji.parse('<:sonks:690184680066252844>')]
-monkey_emojis = [hikari.Emoji.parse('<a:monke:877223400064581633>'),
-                 hikari.Emoji.parse('<a:monkeyspin:668919066056196139>'),
-                 hikari.Emoji.parse('<a:monkeypng:676526278458736728>')]
+monkey_emojis = [hikari.Emoji.parse('<a:monke:1029393246218100876>'),
+                 hikari.Emoji.parse('<a:monkeyspin:1029393241126215711>'),
+                 hikari.Emoji.parse('<a:monkeypng:1029393243542130899>')]
 
 
 @plugin.listener(hikari.GuildMessageCreateEvent)
