38d6886c1a36ebdb2151c689da0b630570dcad2b
Jerryyyz <deligiannisjerry@gmail.com>
2022-10-12 10:26:31 +0100

    Decreased chance to activate

--- bot/extensions/stalk-sonks.py
+++ bot/extensions/stalk-sonks.py
@@ -18,15 +18,18 @@ sonks_replies = ['I will live on forever in your nightmares', 'You are the infer
 @plugin.listener(hikari.GuildMessageCreateEvent)
 async def reply_sonks(event: lightbulb.events) -> None:
     if event.message.author == 293397054116331521:
-        if random.random() < 0.10:
-            print(f'Replied to {event.message.author.username} with a  message')
-            await event.message.respond(random.choice(sonks_replies), reply=True, flags=hikari.MessageFlag.EPHEMERAL)
+        if random.random() < 0.02:
+            reply = random.choice(sonks_replies)
+            print(f'Replied to {event.message.author.username} with a  message: {reply}')
+
+            await event.message.respond(reply, reply=True, flags=hikari.MessageFlag.EPHEMERAL)
 
 
 @plugin.listener(hikari.GuildMessageCreateEvent)
 async def react_sonks(event: lightbulb.events) -> None:
     if event.message.author == 293397054116331521:
-        if random.random() < 0.15:
+        if random.random() < 0.06:
+            print(f'Reacted to {event.message.author.username}s message')
             await event.message.add_reaction('🇰')
             await event.message.add_reaction('🇨')
             await event.message.add_reaction('🇩')
